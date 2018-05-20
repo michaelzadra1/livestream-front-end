3612b5d067eafb5f498e56a7edba46be213fd21e
Michael Zadra <michaelzadra1@gmail.com>
2018-05-20 00:28:28 -0400

    adds logout functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService, SocialUser } from "angularx-social-login";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,22 @@ export class AppComponent implements OnInit {
   user: SocialUser;
   loggedIn: boolean;
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.authService.authState.subscribe((user) => {
       this.user = user;
       this.loggedIn = (user != null);
     });
+  }
+  logout() {
+    this.authService.signOut().then(
+      () => {
+        this.router.navigate(['/']);
+      }
+    );
   }
 }
